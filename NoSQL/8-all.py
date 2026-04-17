7d038dd67cd347c0179710170f2d6ec06f96d0f5
MaxB-Holberton <12107@holbertonstudents.com>
2026-04-17 15:50:06 +1000

    NoSQL updates with pycodestyle

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 """
-	module to list all documents
+    module to list all documents
 """
+
+
 import pymongo
 
+
 def list_all(mongo_collection):
-	"""
-		Function to list all documents
-	"""
+    """
+        Function to list all documents
+    """
     return list(mongo_collection.find())
--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 """
-	module to insert a new document
+    module to insert a new document
 """
+
+
 import pymongo
 
+
 def insert_school(mongo_collection, **kwargs):
-	"""
-		Function to insert a new document
-	"""
+    """
+        Function to insert a new document
+    """
     return mongo_collection.insert(kwargs)
