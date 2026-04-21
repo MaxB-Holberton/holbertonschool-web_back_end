b9b6a167b056e4071b5cede9840801836e5a36d2
MaxB-Holberton <12107@holbertonstudents.com>
2026-04-21 10:54:32 +1000

    linted and completed for checker

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (let item of array) {
-        array[array.indexOf(item)] = appendString + item
-    }
+  const newarray = [];
+  for (const item of array) {
+    newarray[array.indexOf(item)] = appendString + item;
+  }
 
-return array;
+  return newarray;
 }
